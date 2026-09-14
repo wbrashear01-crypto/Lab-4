83aa2ef24598ba31d0f2c96785710ee21e22b4de
wbrashear01-crypto <wbrashear01@saintmarys.edu>
2026-09-14 16:27:44 -0400

    answered
    Updated type hint in docstring from 'true or false' to 'Boolean'.

--- problem1.py
+++ problem1.py
@@ -1,9 +1,10 @@
 def opposite_sign(a,b):
   """
-  integer, integer -> true or false
+  integer, integer -> Boolean
   This function is taking in two integers. If the integer that is inputted for a is greater than the value of the integer
   that is inputted for b, then the function will return true. If the numbers do not fulfill this statement, in all other
   scenarios, the function will return the word false.
+  
   >>> opposite_sign(2,4)
   false
   >>> opposite_sign(4,2)
