1bc9eaf464c41844f3994af1a76497a24fe7b880
wbrashear01-crypto <wbrashear01@saintmarys.edu>
2026-09-14 15:43:00 -0400

    answered
    Add docstring and logic to check opposite signs

--- problem1.py
+++ problem1.py
@@ -1,2 +1,15 @@
 def opposite_sign(a,b):
-  ## your turn
+  """
+  integer, integer -> true or false
+  This function is taking in two integers. If the integer that is inputted for a is greater than the value of the integer
+  that is inputted for b, then the function will return true. If the numbers do not fulfill this statement, in all other
+  scenarios, the function will return the word false.
+  >>> opposite_sign(2,4)
+  false
+  >>> opposite_sign(4,2)
+  true
+  """
+  if a>b:
+    print("true")
+  else:
+    print("false")
