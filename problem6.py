79c9a8cc60ef98b85a128dce589ef3c615174924
wbrashear01-crypto <wbrashear01@saintmarys.edu>
2026-09-14 17:11:55 -0400

    answered

--- problem6.py
+++ problem6.py
@@ -39,10 +39,10 @@ def with_if_function():
   return if_function(c(), t(), f())
 
 def c():
-  ### you write this
+  return False
 
 def t():
-  ### you write this
+  print(1)
 
 def f():
-  ### you write this
+  print(2)
